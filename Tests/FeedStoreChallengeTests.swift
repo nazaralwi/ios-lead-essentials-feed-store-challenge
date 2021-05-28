b2596208cdb697a16727bff50cd54ab4fc8a03b2
Nazar Alwi <alwinazar75@gmail.com>
2021-05-28 10:30:39 +0700

    Move `trackForMemoryLeaks` helper method to a module-level shared scope

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -112,12 +112,6 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
-	
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
     
     private func setupEmptyStoreState() {
         deleteStoreArtifacts()
---
+++ Tests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Nazar Alwi on 28/05/21
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
